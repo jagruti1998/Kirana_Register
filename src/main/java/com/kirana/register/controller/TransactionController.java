678f72926bc01189ab4d7942d6e313b3d9ccf3fe
Jagruti Goswami <jagrutigoswami402@gmail.com>
2024-01-26 16:11:57 +0530

    TransactionController

--- src/main/java/com/kirana/register/controller/TransactionController.java
+++ src/main/java/com/kirana/register/controller/TransactionController.java
@@ -1,11 +1,23 @@
 package com.kirana.register.controller;
 
 
+import com.kirana.register.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/transactions")
 public class TransactionController {
 
+    @Autowired
+    private TransactionService transactionService;
+
+    @GetMapping("/{type}")
+    public ResponseEntity<List<TransactionResponse>> getTransactionsByType(@PathVariable String type) {
+        List<TransactionResponse> transactions = transactionService.getTransactionsByType(type);
+        return ResponseEntity.ok(transactions);
+    }
 }
